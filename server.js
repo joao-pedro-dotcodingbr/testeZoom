d6493bbf0dd44193dd2caf8688eb85a2958c4f5e
Joao Pedro <joao.pedro@dotcoding.com.br>
2023-04-06 16:50:45 -0300

    fix: query

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 
-app.get("/get-code/:code", (req, res, next) => {
+app.get("/get-code", (req, res, next) => {
   try {
 
      const code = req.params?.code;
