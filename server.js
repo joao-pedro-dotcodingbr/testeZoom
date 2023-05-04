b12954082327a34d2c55bb2d018bcb7bdef3383f
Joao Pedro <joao.pedro@dotcoding.com.br>
2023-05-04 09:35:28 -0300

    fix: ajustes

--- server.js
+++ server.js
@@ -6,16 +6,23 @@ app.use(express.json());
 
 
 app.get("/redirect", async (req, res) => {
-  const { code } = req.query;
-  console.log({ query: { code } });
 
-  res.writeHead(301, { Location: `institutofb://MainTab/Documents/${code}` });
+  try {
+     const { code } = req.query;
+     console.log({ query: { code } });
+
+     res.writeHead(301, {
+       Location: `institutofb://MainTab/Documents/${code}`,
+     });
      console.log({ Location: `institutofb://MainTab/Documents/${code}` });
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  res.writeHead(301, { Location: `institutofb://MainTab/Documents/${code}` });
+     await new Promise((resolve) => setTimeout(resolve, 2000));
 
-   console.log({ Location: `institutofb://MainTab/Documents/${code}` });
-  return res.end();
+     console.log({ip : req.ip});
+     return res.end();
+  } catch (error) {
+    console.log(error)
+  }
+
 });;
 
 app.listen(process.env.PORT || 3535, () => {
