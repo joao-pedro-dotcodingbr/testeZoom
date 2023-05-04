18193416b5b11d3f81f3c483be984a2626ca4b66
Joao Pedro <joao.pedro@dotcoding.com.br>
2023-05-04 09:08:05 -0300

    fix: ajuste

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const app = express();
 app.use(express.json());
 
 
-app.get("/redirect", (req, res) => {
+app.get("/redirect", async (req, res) => {
   const { code } = req.query;
   console.log({ query: { code } });
+
   res.writeHead(301, { Location: `institutofb://MainTab/Documents/${code}` });
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  res.writeHead(301, { Location: `institutofb://MainTab/Documents/${code}` });
+
    console.log({ Location: `institutofb://MainTab/Documents/${code}` });
   return res.end();
 });;
